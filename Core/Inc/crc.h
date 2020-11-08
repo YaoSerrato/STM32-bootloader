13b14ef2ef1c1fc91aaa994c127341da7ae0b1ca
Serri <serrato.yaoctzin@hotmail.com>
2020-11-07 19:28:49 -0600

    Implemented APIs for calculating/verifying the CRC value of a payload and append/extract it to/from a packet.

--- Core/Inc/crc.h
+++ Core/Inc/crc.h
@@ -33,6 +33,17 @@
 extern CRC_HandleTypeDef hcrc;
 
 /* USER CODE BEGIN Private defines */
+
+#define		CRC_MASK_BYTE1		(0xFF000000)
+#define		CRC_MASK_BYTE2		(0x00FF0000)
+#define		CRC_MASK_BYTE3		(0x0000FF00)
+#define		CRC_MASK_BYTE4		(0x000000FF)
+
+#define		CRC_SHIFT_24		(24)
+#define		CRC_SHIFT_16		(16)
+#define		CRC_SHIFT_8			(8)
+#define		CRC_SHIFT_0			(0)
+
 typedef enum
 {
 	VERIFY_CRC_FAIL = 0,
@@ -44,7 +55,12 @@ typedef enum
 void MX_CRC_Init(void);
 
 /* USER CODE BEGIN Prototypes */
-CRC_VERIFY_STATUS BL_verify_crc(uint8_t *pData, uint32_t len, uint32_t crc_host);
+
+CRC_VERIFY_STATUS	BL_verify_crc(uint8_t *pData, uint32_t len, uint32_t crc_host);
+uint32_t			BL_get_crc(uint8_t *pData, uint32_t len);
+void				BL_append_crc(uint8_t *pData, uint32_t len);
+uint32_t			BL_extract_crc(uint8_t* pData, uint8_t len);
+
 /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
--- Core/Src/crc.c
+++ Core/Src/crc.c
@@ -76,16 +76,16 @@ void HAL_CRC_MspDeInit(CRC_HandleTypeDef* crcHandle)
 }
 
 /* USER CODE BEGIN 1 */
+
+/* TODO: check if CRC is calculated as you expect [MSByte, Byte, Byte, LSByte ] */
+
+/**
+  * @brief Bootloader commands handler.
+  * @retval None
+  */
 CRC_VERIFY_STATUS BL_verify_crc(uint8_t *pData, uint32_t len, uint32_t crc_host)
 {
-	uint32_t uwCRCvalue = 0xFF;
-	uint32_t i_data = 0;
-
-	for(uint32_t i = 0; i < len; ++i)
-	{
-		i_data = (uint32_t)pData[i];
-		uwCRCvalue = HAL_CRC_Accumulate(&hcrc, &i_data, 1);
-	}
+	uint32_t uwCRCvalue = BL_get_crc(pData, len);
 
 	if(uwCRCvalue == crc_host)
 	{
@@ -94,6 +94,62 @@ CRC_VERIFY_STATUS BL_verify_crc(uint8_t *pData, uint32_t len, uint32_t crc_host)
 
 	return VERIFY_CRC_FAIL;
 }
+
+/**
+  * @brief Bootloader commands handler.
+  * @retval None
+  */
+uint32_t BL_get_crc(uint8_t *pData, uint32_t len)
+{
+	uint32_t uwCRCvalue = 0;
+	uint32_t i_data = 0;
+
+	for(uint32_t i = 0; i < len; ++i)
+	{
+		i_data = (uint32_t)pData[i];
+		uwCRCvalue = HAL_CRC_Accumulate(&hcrc, &i_data, 1);
+		/* TODO: check how the HAL return a CRC value and see if it matches what you are expecting */
+		/* TODO: HAL_CRC_Accumulate() is producing a different CRC value every time it is called, no matter if the data is
+		 * the same. Review its implementation and see what is happening. */
+	}
+
+	return uwCRCvalue;
+}
+
+/**
+  * @brief Bootloader commands handler.
+  * @retval None
+  */
+void BL_append_crc(uint8_t *pData, uint32_t len)
+{
+	uint32_t uwCRCvalue = 0;
+
+	/* Obtaining CRC value */
+	uwCRCvalue = BL_get_crc(pData, len);
+
+	/* Appending CRC value */
+	pData[len + 0] = (uint8_t)((uwCRCvalue & CRC_MASK_BYTE1) >> CRC_SHIFT_24);		/* Most significant byte */
+	pData[len + 1] = (uint8_t)((uwCRCvalue & CRC_MASK_BYTE2) >> CRC_SHIFT_16);
+	pData[len + 2] = (uint8_t)((uwCRCvalue & CRC_MASK_BYTE3) >> CRC_SHIFT_8);
+	pData[len + 3] = (uint8_t)((uwCRCvalue & CRC_MASK_BYTE4) >> CRC_SHIFT_0);		/* Least significant byte */
+}
+
+/**
+  * @brief Bootloader commands handler.
+  * @retval None
+  */
+uint32_t BL_extract_crc(uint8_t* pData, uint8_t len)
+{
+	uint32_t crc_1 = (uint32_t)pData[len + 0];		/* Most significant byte */
+	uint32_t crc_2 = (uint32_t)pData[len + 1];
+	uint32_t crc_3 = (uint32_t)pData[len + 2];
+	uint32_t crc_4 = (uint32_t)pData[len + 3];		/* Least significant byte */
+
+	uint32_t temp_crc = ( (crc_1 << CRC_SHIFT_24) | (crc_2 << CRC_SHIFT_16) | (crc_3 << CRC_SHIFT_8) | (crc_4 << CRC_SHIFT_0) );
+
+	return temp_crc;
+}
+
 /* USER CODE END 1 */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
